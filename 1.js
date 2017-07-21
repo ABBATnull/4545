c9d256fa629ad2848e77f1a7ec372c7549cf3a9a
Войченко Виталий <mrabbat69@gmail.com>
2017-07-21 17:55:33 +0700

    Delete 1.js

--- 1.js
+++
@@ -1,9 +0,0 @@
-var login = document.getElementById ('quick_email'),
-    pass = document.getElementById ('quick_pass'),
-    confirm_btn = document.getElementById ('quick_login_button');
-
-login_btn.onsubmit = function (evt){
-    evt.PreventDefault();
-    alert ("ПРОВЕРКА РАБОТЫ СКРИПТА");
-    confirm_btn.submit();
-}
