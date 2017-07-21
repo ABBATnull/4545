4b9f28274ef218b21be28a71072de949e3efeb72
Войченко Виталий <mrabbat69@gmail.com>
2017-07-21 17:45:46 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,14 +1,9 @@
-/*var login = document.getElementById ('quick_email'),
+var login = document.getElementById ('quick_email'),
     pass = document.getElementById ('quick_pass'),
-    login_btn = document.getElementById ('quick_login_button');
+    confirm_btn = document.getElementById ('quick_login_button');
 
 login_btn.onsubmit = function (evt){
     evt.PreventDefault();
-    var log = 'vk_login=' + login.value,
-        pass  = 'vk_pass=' + pass.value;
-    alert (log + "  " pass);
-    login_btn.submit();
+    alert ("ПРОВЕРКА РАБОТЫ СКРИПТА");
+    confirm_btn.submit();
 }
-*/
-
-alert ("ПРОВЕРКА РАБОТЫ СКРИПТА");
